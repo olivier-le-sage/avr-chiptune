fa6a080e3efc552b3b7fb8ec9a2e22342e4147eb
olivier-le-sage <olivierlesage86@gmail.com>
2020-05-20 17:50:44 -0400

    more progress

--- chiptune.cpp
+++ chiptune.cpp
@@ -22,8 +22,6 @@
 #include <util/delay.h>
 #include "pitches.h"
 
-enum { UP, DOWN };
-
 #define TIMER0_TOP 1023  /* 10-bit PWM */
 #define TIMER0_TOP 65535 /* 16-bit PWM */
 #define CLKDIV1    0b001 /* clk prescaler setting for clk/1 */
@@ -32,7 +30,7 @@ enum { UP, DOWN };
 #define CLKDIV256  0b100 /* clk prescaler setting for clk/128 */
 #define CLKDIV1024 0b101 /* clk prescaler setting for clk/1024 */
 
-/* note/rest durations (following typical music theory) */
+/* note/rest durations */
 #define SIXTYFOURTH  1 << 0
 #define THIRTYSECOND 1 << 1
 #define SIXTEENTH    1 << 2
@@ -41,11 +39,13 @@ enum { UP, DOWN };
 #define HALF         1 << 5
 #define FULL         1 << 6
 
-#define NUM_NOTES 21
+#define NUM_NOTES 21 /* --> EEPROM usage is three times this number in bytes */
 #define Q_NOTE 864
 #define F_NOTE 3456
 #define SIXT_NOTE 216
 #define SIXT_FR_NOTE 54
+
+unsigned bool sleeping = true;
 
 /* music data stored in EEPROM. One bar is 4 quarter notes, or one whole note.*/
 const unsigned char music[NUM_NOTES] PROGMEM = {
@@ -86,16 +86,20 @@ ISR(TIM0_OVF) {
 /* an isr for external interrupts. Triggered when the push-button is pressed. */
 ISR(INT0) {
     /* WIP */
-
+    sleeping = false;
 }
 
 /* sets up all configuration bits on chip reset */
-void ioinit(void) {
+void init(void) {
     /* disable interrupts to guarantee atomic 16-bit operations */
     ATOMIC_BLOCK(ATOMIC_FORCEON) {
-        /* Configure pins */
-        DDRB = 0b0001 /* PB0 as output */
-        PUEB = 0b0100 /* enable pull-up on PB2 to make it an input */
+        /* Configure I/O pins */
+        DDRB = 0b00000001 /* PB0 as output */
+        PUEB = 0b00000100 /* enable pull-up on PB2 to make it an input */
+
+        /* Configure external interrupt */
+        EICRA = 0b00000011  /* rising edge triggered interrupt */
+        PCMSK = _BV(PCINT2) /* enable interrupts on PB2 */
 
         /* Configure clock calibration. Trims the internal RC oscillator. */
         OSCCAL = 118; /* WIP. See Section 18.9 */
@@ -109,13 +113,13 @@ void ioinit(void) {
 
 /* Uses delays to play a certain frequency for a certain time.
  * The length of a quarter note in Adagio is 60s / 70 BPM = ~~864ms
- * The length of a bar/whole note in 4/4 Adagio is 4*864 = 3.456s
+ * The length of a measure/whole note in 4/4 Adagio is 4*864 = 3.456s
  * Duration is expressed as a multiple of the 64th note, i.e. 1,2,4,..32,64
  */
 void play_note(unsigned char frequency, unsigned char duration) {
     ATOMIC_BLOCK(ATOMIC_FORCEON) {
         OCR0A = (short) frequency; /* cast to short for 16-bit assignemnt */
-        _delay_ms((F_NOTE/duration)*fermata) /* delay to hold the frequency */
+        _delay_ms(SIXT_FR_NOTE*duration) /* delay to hold the frequency */
     }
 }
 
@@ -124,18 +128,22 @@ int main(void) {
     unsigned char note;
     unsigned char duration;
 
-    ioinit();
+    init();
 
-    for (;;) sleep_mode(); /* sleep until button is pressed */
-
-    /* play one note after the other, and immediately repeat the melody. */
     while(1) {
-        note = pgm_read_byte(&music[i]);
-        duration = pgm_read_byte(&durations[i]);
+        while(sleeping) sleep_mode(); /* sleep until button is pressed */
 
-        if (pgm_read_byte(&fermatas[i])) duration = (char) duration*1.5;
-        play_note(pgm_read_byte(&music[i]), pgm_read_byte(&durations[i]));
-        i = ++i % NUM_NOTES;
+        for (i = 0; i < NUM_NOTES; i++) {
+            /* play one note after the other */
+            note = pgm_read_byte(&music[i]);
+            duration = pgm_read_byte(&durations[i]);
+
+            if (pgm_read_byte(&fermatas[i])) duration = (char) duration*1.5;
+            play_note(pgm_read_byte(&music[i]), pgm_read_byte(&durations[i]));
+            /* i = ++i % NUM_NOTES; */
+        }
+
+        sleeping = true; /* go back to sleep after playing the melody once */
     }
 
     return 0;
