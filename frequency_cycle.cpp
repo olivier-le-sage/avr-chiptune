661b9a0c4bf408cf1eab0789dde1dd247d50e70b
olivier-le-sage <olivierlesage86@gmail.com>
2020-05-28 15:48:07 -0400

    added "control group" code for testing

---
+++ frequency_cycle.cpp
@@ -0,0 +1,19 @@
+
+#include <inttypes.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <util/atomic.h>
+
+int main (void) {
+    OSCCAL = 118;
+    DDRB = 1; // PB0 output
+    TCCR0A = _BV(WGM00) | _BV(WGM01) | _BV(COM0A0);
+    TCCR0B = _BV(CS02)  | _BV(WGM02) | _BV(WGM03);
+
+    for (;;) {
+        ATOMIC_BLOCK(ATOMIC_FORCEON) {
+            OCR0A += 10;
+        }
+        _delay_ms(10);
+    }
+}
