691d5db4c799ad96b383060c9b380cddf4ea82c4
olivier-le-sage <olivierlesage86@gmail.com>
2020-05-28 22:56:19 -0400

    switched back to Fast PWM and tweaked prescale + timing

--- frequency_cycle.cpp
+++ frequency_cycle.cpp
@@ -4,17 +4,23 @@
 #include <util/delay.h>
 #include <util/atomic.h>
 
+#define CLKDIV1    0b001 /* clk prescaler setting for clk/1 */
+#define CLKDIV8    0b010 /* clk prescaler setting for clk/8 */
+#define CLKDIV64   0b011 /* clk prescaler setting for clk/64 */
+#define CLKDIV256  0b100 /* clk prescaler setting for clk/256 */
+#define CLKDIV1024 0b101 /* clk prescaler setting for clk/1024 */
+
 int main (void) {
     OSCCAL = 118;
     DDRB = 1; // PB0 output
-    TCCR0A = _BV(COM0A0) | _BV(WGM01);
-    TCCR0B = _BV(CS00); // clk/1
+    TCCR0A = _BV(WGM00) | _BV(WGM01) | _BV(COM0A0);
+    TCCR0B = CLKDIV64 | _BV(WGM02) | _BV(WGM03);
     OCR0A = 0; // start pwm at max frequency
 
     for (;;) {
         ATOMIC_BLOCK(ATOMIC_FORCEON) {
             OCR0A += 10; // gradually bring the frequency down (by ++ing period)
         }
-        _delay_ms(10);
+        _delay_ms(500);
     }
 }
